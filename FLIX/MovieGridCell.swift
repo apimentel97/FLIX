ec3cddb2c4e4fab54f5b5143f207f7dd51501b53
Alejandro Pimentel <apimentel97@yahoo.com>
2019-02-24 19:53:26 -0800

    Finished with part 2

---
+++ FLIX/MovieGridCell.swift
@@ -0,0 +1,15 @@
+//
+//  MovieGridCell.swift
+//  FLIX
+//
+//  Created by Alejandro pimentel on 2/19/19.
+//  Copyright © 2019 Alejandro pimentel. All rights reserved.
+//
+
+import UIKit
+
+class MovieGridCell: UICollectionViewCell {
+   
+    @IBOutlet weak var posterView: UIImageView!
+    
+}
